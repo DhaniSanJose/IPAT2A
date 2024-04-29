3820b7e63e1e94e219bb813e2526645d5572704a
DhaniSanJose <furrdoy@gmail.com>
2024-04-29 17:51:59 +0800

    working login

--- dcrm/urls.py
+++ dcrm/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('mypage', views.home, name='home'),
+    path('', views.home, name='home'),
     path('register', views.register, name='register'),
+    path('logout/', views.logout_user, name='logout'),
 ]

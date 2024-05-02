227f0e1c50a60feea89b4c1204582d4933e72cbd
DhaniSanJose <sanjose.dhani@gmail.com>
2024-05-02 14:59:50 +0800

    Working CRUD

--- dcrm/views.py
+++ dcrm/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .models import Record
 
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 # Create your views here.
 
 
@@ -49,3 +49,49 @@ def logout_user(request):
     logout(request)
     messages.success(request, "Your have been logged out...")
     return redirect('home')
+
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record Has Been Updated")
+            return redirect('home')
+        return render(request, 'update_record.html',{'form':form} )
+    else:
+         messages.success(request, "You must Be Logged In First...")
+         return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record Has Been Deleted")
+        return redirect('home')
+    else:
+         messages.success(request, "You must Be Logged In First...")
+         return redirect('home')
+
+
+
+
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added..")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "You must Be Logged In First...")
+        return redirect('home')
+
+
+
